77acddac467724109044b3a7a87469493f375744
avigunjal07 <avishkargunjal07@gmail.com>
2026-06-16 22:06:57 +0530

    Left Pyramid pattern

---
+++ LeftPyramid.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class LeftPyramid{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter Height : ");
+        int hei=sc.nextInt();
+
+        for(int i=1;i<=hei;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        return;
+    }
+}
