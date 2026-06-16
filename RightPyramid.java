2b95cfb8d1273de7f86546b561c8c70372962066
avigunjal07 <avishkargunjal07@gmail.com>
2026-06-16 22:22:44 +0530

    Right Pyramid pattern

---
+++ RightPyramid.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class RightPyramid{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+
+        System.out.print("Enter Height : ");
+        int hei=sc.nextInt();
+
+        for(int i=1;i<=hei;i++){
+            for(int j=1;j<=hei-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print("X");
+            }
+
+            System.out.println();
+        }
+
+        return;
+    }
+}
